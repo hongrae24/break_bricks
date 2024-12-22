be06dee1265c2ddac1b4313b326204eacf80a783
hongrae24 <hongrae24@gmail.com>
2024-12-23 01:23:14 +0900

    Draw brick count

--- brick.py
+++ brick.py
@@ -4,6 +4,7 @@ BRICK_WIDTH = 100
 BRICK_HEIGHT = 50
 
 BRICK_COLOR = (255, 0, 0)
+BRICK_TEXT_COLOR = (0, 0, 0)
 
 class Brick:
     def __init__(self, lt_pos, count):
@@ -12,4 +13,12 @@ class Brick:
         self.count = count
     
     def draw(self, window):
-        pygame.draw.rect(window, BRICK_COLOR, self.rect)        
+        pygame.draw.rect(window, BRICK_COLOR, self.rect) 
+        
+        font = pygame.font.SysFont("Comic Sans MS", 30)
+        text_surf = font.render("{}".format(self.count), True, (0, 0, 0))
+        text_pos = (self.rect.centerx - text_surf.get_width() / 2 , self.rect.centery - text_surf.get_height() / 2)
+        window.blit(text_surf, text_pos)  
+
+    def update(self, window):
+        pass     

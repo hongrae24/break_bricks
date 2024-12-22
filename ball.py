fc7cc2a0285a59d06fd3a1e7d9e80c16aa021b40
hongrae24 <hongrae24@gmail.com>
2024-12-23 01:14:07 +0900

    Use pygame math lib

--- ball.py
+++ ball.py
@@ -1,16 +1,15 @@
 import pygame
-import numpy as np
 
 BALL_COLOR = (0, 0, 255)
 BALL_RADIUS = 13
 
 class Ball:
     def __init__(self, x, y):
-        self.pos = np.array([x, y])
-        self.move = np.array([0, 0])
+        self.pos = pygame.math.Vector2(x, y)
+        self.move = pygame.math.Vector2(0, 0)
     
     def draw(self, window):
-        pygame.draw.circle(window, BALL_COLOR, (self.pos[0], self.pos[1]), BALL_RADIUS)
+        pygame.draw.circle(window, BALL_COLOR, (self.pos.x, self.pos.y), BALL_RADIUS)
     
     def update(self):
         pass
